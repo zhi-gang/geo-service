3d74cc441cc2883dc7ce3488472654c5b154c917
Zhigang <zhi-gang.yan@outlook.com>
2025-01-20 18:46:26 +0800

    v1

--- src/main/java/com/example/geoservice/controller/GeoController.java
+++ src/main/java/com/example/geoservice/controller/GeoController.java
@@ -3,6 +3,9 @@ package com.example.geoservice.controller;
 import com.example.geoservice.model.Location;
 import com.example.geoservice.service.MapService;
 import com.example.geoservice.service.MapServiceFactory;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
+@Tag(name = "地理服务接口", description = "提供地理位置相关的服务，如驾车距离计算等")
 @RestController
 @RequestMapping("/api/geo")
 public class GeoController {
@@ -20,8 +24,11 @@ public class GeoController {
         this.mapServiceFactory = mapServiceFactory;
     }
 
+    @Operation(summary = "计算驾车距离", description = "计算两个位置坐标之间的驾车距离，返回单位为米")
     @PostMapping("/distance")
-    public double calculateDrivingDistance(@RequestBody List<Location> locations) throws IOException {
+    public double calculateDrivingDistance(
+            @Parameter(description = "位置坐标列表，必须包含两个位置坐标，分别表示起点和终点")
+            @RequestBody List<Location> locations) throws IOException {
         if (locations.size() != 2) {
             throw new IllegalArgumentException("必须提供两个位置坐标");
         }
